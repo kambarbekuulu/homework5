8fba825253be3251c49182cfb2c59ee697aa2788
imarov11 <saidibakaskambarbekovich@gmail.com>
2022-03-22 16:13:23 +0300

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        for (int i = 4; i < 10; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+
+
+            }
+        }
+    }
+}
